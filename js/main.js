6ef650f0d27a2c2f8e813bf607169528d7ef3d8c
Lukas <luki.brugger@gmail.com>
2023-03-09 18:41:10 +0100

    Decently finished the menu, wordle starts now

--- js/main.js
+++ js/main.js
@@ -3,49 +3,71 @@ const elementw = document.querySelector('#sliderw')
 const texth = document.querySelector('#texth')
 const textw = document.querySelector('#textw')
 
-var posh = elementh.getBoundingClientRect();
-var posw = elementw.getBoundingClientRect();
-var hWidth = posh.right-posh.left
-var wWidth = posw.right-posw.left
-var Stepsh = hWidth / 8
-var Stepsw = wWidth / 5
+var posh
+var posw
+var hWidth
+var wWidth
+var Stepsh
+var Stepsw
+var valueh = 3
+var valuew = 2
+
+function relcalch() {
+    var relPos2 = valueh * Stepsh
+    elementh.style.setProperty("--relative_pos",`${relPos2}px`)
+    texth.innerHTML = valueh +3
+}
+
+function relcalcw() {
+    var relPos2 = valuew * Stepsw
+    elementw.style.setProperty("--relative_pos",`${relPos2}px`)
+    textw.innerHTML = valuew +3
+}
+
+function refresh() {
+    posh = elementh.getBoundingClientRect();
+    posw = elementw.getBoundingClientRect();
+    hWidth = posh.right-posh.left
+    wWidth = posw.right-posw.left
+    console.log(hWidth)
+    Stepsh = hWidth / 8
+    Stepsw = wWidth / 5
+    relcalch(valueh)
+    relcalcw(valuew)
+}
+
+window.onresize = refresh
+window.onload = refresh
 
 elementh.addEventListener("mousedown", () => {
-    posh = elementh.getBoundingClientRect();
+    refresh();
     elementh.addEventListener("mousemove", handleEventh);
 })
     
 elementh.addEventListener("mouseup", () => {
-    posh = elementh.getBoundingClientRect();
+    refresh();
     elementh.removeEventListener("mousemove", handleEventh);
 })
 
 elementw.addEventListener("mousedown", () => {
-    posw = elementw.getBoundingClientRect();
+    refresh();
     elementw.addEventListener("mousemove", handleEventw);
 })
     
 elementw.addEventListener("mouseup", () => {
-    posw = elementw.getBoundingClientRect();
+    refresh();
     elementw.removeEventListener("mousemove", handleEventw);
 })
 
 
 function handleEventh(e){
     var relPos = e.clientX + 10 - posh.left
-    var value = Math.floor(relPos / Stepsh)
-    var relPos2 = value * Stepsh
-    elementh.style.setProperty("--relative_pos",`${relPos2}px`)
-
-    texth.innerHTML = value + 3
-
+    valueh = Math.floor(relPos / Stepsh)
+    relcalch(valueh)
 }
 
 function handleEventw(e){
     var relPos = e.clientX +10 - posw.left
-    var value = Math.floor(relPos / Stepsw)
-    var relPos2 = value * Stepsw
-    elementw.style.setProperty("--relative_pos",`${relPos2}px`)
-
-    textw.innerHTML = value +3
+    valuew = Math.floor(relPos / Stepsw)
+    relcalcw(valuew)
 }
