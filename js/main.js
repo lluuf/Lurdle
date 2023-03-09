7715d0555fcd8a4805d752cd1db3ef00e22b8329
Lukas <luki.brugger@gmail.com>
2023-03-09 15:12:16 +0100

    Added sliding

--- js/main.js
+++ js/main.js
@@ -1,8 +1,51 @@
-const element = document.getElementById('height_sl')
+const elementh = document.querySelector('#sliderh')
+const elementw = document.querySelector('#sliderw')
+const texth = document.querySelector('#texth')
+const textw = document.querySelector('#textw')
 
-// always checking if the element is clicked, if so, do alert('hello')
-element.addEventListener("mousedown", () => {
-    element.addEventListener("mousemove", (e) => {
-        console.log(`Mouse X: ${e.clientX}`);
-    });
-});
+var posh = elementh.getBoundingClientRect();
+var posw = elementw.getBoundingClientRect();
+var hWidth = posh.right-posh.left
+var wWidth = posw.right-posw.left
+var Stepsh = hWidth / 8
+var Stepsw = wWidth / 5
+
+elementh.addEventListener("mousedown", () => {
+    posh = elementh.getBoundingClientRect();
+    elementh.addEventListener("mousemove", handleEventh);
+})
+    
+elementh.addEventListener("mouseup", () => {
+    posh = elementh.getBoundingClientRect();
+    elementh.removeEventListener("mousemove", handleEventh);
+})
+
+elementw.addEventListener("mousedown", () => {
+    posw = elementw.getBoundingClientRect();
+    elementw.addEventListener("mousemove", handleEventw);
+})
+    
+elementw.addEventListener("mouseup", () => {
+    posw = elementw.getBoundingClientRect();
+    elementw.removeEventListener("mousemove", handleEventw);
+})
+
+
+function handleEventh(e){
+    var relPos = e.clientX + 10 - posh.left
+    var value = Math.floor(relPos / Stepsh)
+    var relPos2 = value * Stepsh
+    elementh.style.setProperty("--relative_pos",`${relPos2}px`)
+
+    texth.innerHTML = value + 3
+
+}
+
+function handleEventw(e){
+    var relPos = e.clientX +10 - posw.left
+    var value = Math.floor(relPos / Stepsw)
+    var relPos2 = value * Stepsw
+    elementw.style.setProperty("--relative_pos",`${relPos2}px`)
+
+    textw.innerHTML = value +3
+}
